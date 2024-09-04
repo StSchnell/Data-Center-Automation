d49b5be7dc6d9542e7da4184ff24b98ff34e2db6
Stefan Schnell <130429818+StSchnell@users.noreply.github.com>
2024-09-04 06:16:26 +0200

    Update invokeTypeScript.js

--- JavaScriptTools/TypeScript/addNumbers/invokeTypeScript.js
+++ JavaScriptTools/TypeScript/addNumbers/invokeTypeScript.js
@@ -51,27 +51,88 @@ var _invokeTypeScriptNS = {
         System.log(typescriptSource);
       }
 
-      var transpileResult = System.getModule("de.stschnell.typescript")
-        .transpileTypeScriptToJavaScript(typescriptSource, actionName);
+      if (typescriptSource.trim() !== "") {
 
-      // Executes the JavaScript
-      var scope = cx.initStandardObjects();
-      var sourceName = "export";
-      var lineNumber = 1;
-      var securityDomain = null;
+        // Transpiles the TypeScript to JavaScript
+        var transpileResult = System.getModule("de.stschnell.typescript")
+          .transpileTypeScriptToJavaScript(typescriptSource, actionName);
+        if (showOutput) {
+          System.log(transpileResult.diagnosticMessages);
+        }
+        var javascriptSource = transpileResult.javascriptSource;
 
-      var result = cx.evaluateString(
-        scope,
-        javascriptSource,
-        sourceName,
-        lineNumber,
-        securityDomain
-      );
-      if (showOutput) {
-        System.log(String(result));
+        // Add input parameters to JavaScript code
+        if (parameters.trim() !== "") {
+          var addParameters = "";
+          var jsonParameters = JSON.parse(parameters);
+          Object.keys(jsonParameters).forEach( function(key) {
+            var parameter = "var " + key + " = " + jsonParameters[key] + ";";
+            addParameters += parameter + "\n";
+          });
+          javascriptSource = addParameters + "\n" + javascriptSource;
+        }
+
+        if (showOutput) {
+          System.log(javascriptSource);
+        }
+
+        // Executes the JavaScript
+        var scope = cx.initStandardObjects();
+        var sourceName = "export";
+        var lineNumber = 1;
+        var securityDomain = null;
+
+        var result = cx.evaluateString(
+          scope,
+          javascriptSource,
+          sourceName,
+          lineNumber,
+          securityDomain
+        );
+        if (showOutput) {
+            switch (typeof result) {
+            case "string" :
+            case "number" :
+            case "boolean" :
+              System.log(String(result));
+              break;
+            case "function" :
+              break;
+            case "object" :
+              if (result.constructor !== undefined) {
+                switch (result.constructor.name) {
+                  case "String" :
+                  case "Number" :
+                  case "Boolean" :
+                    System.log(result);
+                    break;
+                  case "Array" :
+                    System.log("Array: " + String(result));
+                    break;
+                  case "Date" :
+                    System.log("Date: " + String(result));
+                    break;
+                  case "Function" :
+                    break;
+                  case "Object" :
+                    System.log(JSON.stringify(result));
+                    break;
+                  case "RegExp" :
+                    System.log("RegExp: " + String(result));
+                    break;
+                  default :
+                    break;
+                }
+              }
+              break;
+            default :
+              break;
+          }
+        }
+
+        return result;
+
       }
-
-      return String(result);
 
     } catch (exception) {
       System.log(exception);
