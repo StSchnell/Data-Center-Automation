a733fed2bb7b0f5df484a901ffab73943d0e4ee3
Stefan Schnell <130429818+StSchnell@users.noreply.github.com>
2024-09-04 06:02:50 +0200

    Update handler.js

--- JavaScriptTools/TypeScript/addNumbers/handler.js
+++ JavaScriptTools/TypeScript/addNumbers/handler.js
@@ -1,15 +1,17 @@
 // Begin ---------------------------------------------------------------
 
 /**
+ * Handler to call the transpilation.
+ *
  * @param {string} typescriptSource
  * @param {string} actionName
  * @returns {Properties}
  *
  * @author Stefan Schnell <mail@stefan-schnell.de>
  * @license MIT
- * @version 0.4.0
+ * @version 0.7.0
  *
- * Checked with Aria Automation 8.13.1
+ * Checked with Aria Automation 8.13.1, 8.16.0 and 8.18.0
  */
 
 exports.handler = (context, inputs, callback) => {
@@ -20,12 +22,14 @@ exports.handler = (context, inputs, callback) => {
   const fs = require("node:fs");
   const typescript = require('./typescript.js');
 
+  // Compiler options for transpilation to JavaScript for the Rhino engine
   const compilerOptions = {
     module: typescript.ModuleKind.CommonJS,
     target: typescript.ScriptTarget.ES5,
     strictNullChecks: true
   };
 
+  // Checking the TypeScript code and error output if present
   const inMemoryFileName = `${os.tmpdir()}/${inputs.actionName}.ts`;
   try {
     fs.accessSync(fileName, fs.constants.F_OK);
@@ -35,12 +39,13 @@ exports.handler = (context, inputs, callback) => {
 
   const host = typescript.createCompilerHost(compilerOptions);
   host.writeFile(inMemoryFileName, inputs.typescriptSource);
-  const program = typescript.createProgram([inMemoryFileName], compilerOptions, host);
+  const program = 
+    typescript.createProgram([inMemoryFileName], compilerOptions, host);
   const emitResult = program.emit();
   const allDiagnostics =
     typescript.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+  let diagnosticMessages = "";
   if (allDiagnostics.length > 0) {
-    console.log("");
     allDiagnostics.forEach(diagnostic => {
       if (diagnostic.file) {
         let { line, character } = typescript.getLineAndCharacterOfPosition(
@@ -51,18 +56,18 @@ exports.handler = (context, inputs, callback) => {
           diagnostic.messageText,
           "\n"
         );
-        console.log(`[Line ${line + 1}, Column ${character + 1}] ${message}`);
+        diagnosticMessages +=
+          `[Line ${line + 1}, Column ${character + 1}] ${message}\n`;
       } else {
-        console.log(
-          typescript.flattenDiagnosticMessageText(
-            diagnostic.messageText,
-            "\n"
-          )
+        diagnosticMessages += typescript.flattenDiagnosticMessageText(
+          diagnostic.messageText,
+          "\n"
         );
       }
     });
   }
 
+  // Transpile TypeScript to JavaScript
   const javascriptSource = typescript.transpileModule(
     inputs.typescriptSource,
     compilerOptions
@@ -70,7 +75,8 @@ exports.handler = (context, inputs, callback) => {
 
   callback(undefined, {
     status: "done",
-    javascriptSource: javascriptSource.outputText
+    javascriptSource: javascriptSource.outputText,
+    diagnosticMessages: diagnosticMessages
   });
 
 };
