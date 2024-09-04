bcdd515d294b5a2406d3998ca6abe4689999482d
Stefan Schnell <130429818+StSchnell@users.noreply.github.com>
2024-09-04 06:18:49 +0200

    Update testTypeScript.ts

--- JavaScriptTools/TypeScript/addNumbers/testTypeScript.ts
+++ JavaScriptTools/TypeScript/addNumbers/testTypeScript.ts
@@ -1,14 +1,35 @@
 // Begin ---------------------------------------------------------------
 
-function addNumbers(a: number, b: number) {
+/**
+ * @param in_name {string}
+ * @param in_age {number}
+ * @returns {string}
+ */
+
+function addNumbers(a: number, b: number) : number {
   return a + b;
 }
 
-function main() {
-  const sum: number = addNumbers(10, 15);
-  return "Sum of the two numbers is: " + sum;
+let subNumbers = (a: number, b: number) : number => {
+  return a - b;
 }
 
-main();
+const getHelloWorld = (name: string, age: number) : string => {
+  return `Hello World from ${name} with the age ${age.toString()}`;
+}
+
+function main(name: string, age: number) {
+
+  const sum: number = addNumbers(10, 15);
+  const diff: number = subNumbers(15, 10);
+  const hello: string = getHelloWorld(name, age);
+
+  return "Sum of the two numbers is: " + sum + "\n" +
+    "Diff of the two numbers is: " + diff + "\n" +
+    hello;
+
+}
+
+main(in_name, in_age);
 
 // End -----------------------------------------------------------------
